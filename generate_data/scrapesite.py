e161517b631987435885de2e7bdd53fd2f83d606
17hogeju <17hogeju@gmail.com>
2023-01-06 17:28:41 -0800

    clean data

--- generate_data/scrapesite.py
+++ generate_data/scrapesite.py
@@ -7,13 +7,17 @@ import json
 # url = 'https://www.rottentomatoes.com/browse/tv_series_browse/affiliates:hbo_max~sort:popular?page=5' # hbo max shows
 # url = 'https://www.rottentomatoes.com/browse/tv_series_browse/affiliates:amazon_prime~sort:popular?page=5' # amazon prime shows
 # url = 'https://www.rottentomatoes.com/browse/tv_series_browse/affiliates:disney_plus~sort:popular?page=5' # disney plus shows
-# url = 'https://www.rottentomatoes.com/browse/tv_series_browse/affiliates:apple_tv_plus~sort:popular?page=5' # apple plus shows
 
 
-# url = 'https://www.rottentomatoes.com/browse/movies_at_home/affiliates:netflix~sort:popular?page=5' # netflix movies
+
 # url = 'https://www.rottentomatoes.com/browse/movies_at_home/affiliates:hulu~sort:popular?page=5' # hulu movies
+# url = 'https://www.rottentomatoes.com/browse/movies_at_home/affiliates:netflix~sort:popular?page=5' # netflix movies
 # url = 'https://www.rottentomatoes.com/browse/movies_at_home/affiliates:hbo_max~sort:popular?page=5' # hbo max movies
+# url = 'https://www.rottentomatoes.com/browse/movies_at_home/affiliates:amazon_prime~sort:popular?page=5' # amazon prime movies
 url = 'https://www.rottentomatoes.com/browse/movies_at_home/affiliates:disney_plus~sort:popular?page=5' # disney plus movies
+
+affiliate = "disney"
+ttype = "movie"
 
 
 def get_data(url):
@@ -27,11 +31,15 @@ soup = BeautifulSoup(temp, 'lxml')
 
 results = soup.find(id="main-page-content")
 items = results.find_all("img", alt=True)
-res = []
+titles = []
 
 for item in items:
-    res.append(item['alt'])
+    titles.append(item['alt'])
 
-
-with open('../data/disney_movies.json', 'w') as f:
+res = {
+    "affiliate": affiliate,
+    "ttype": ttype,
+    "titles": titles
+}
+with open(f'../data/{affiliate}_{ttype}.json', 'w') as f:
     json.dump(res, f)
