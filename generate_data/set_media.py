4b66bf0c6018d8f151713d9f34c9ebea96d2ffc7
17hogeju <17hogeju@gmail.com>
2023-02-07 15:11:00 -0800

    typo

--- generate_data/set_media.py
+++ generate_data/set_media.py
@@ -3,7 +3,7 @@ import json
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate('../firebase-credentials.json')
+cred = credentials.Certificate('../firebase_credentials.json')
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
