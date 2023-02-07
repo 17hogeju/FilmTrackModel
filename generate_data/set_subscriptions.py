8c18c6c7ba127590129090760facdffd4a3c8131
17hogeju <17hogeju@gmail.com>
2023-02-07 15:05:34 -0800

    Modify credentials

--- generate_data/set_subscriptions.py
+++ generate_data/set_subscriptions.py
@@ -3,7 +3,7 @@ import json
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate('../film-track-05276ec63da6.json')
+cred = credentials.Certificate('../firebase-credentials.json')
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
