09367fec77e8418ff22a3a50dac794ad2dcc53ac
Paul-Cui-Bugkiller <84996892+Paul-Cui-Bugkiller@users.noreply.github.com>
2023-02-13 16:44:28 -0500

    Update String_Catching_Algorithm.py

--- String_Catching_Algorithm.py
+++ String_Catching_Algorithm.py
@@ -1,4 +1,4 @@
-﻿#We use Rabin-Karp algorithm for acoomplishing our string matching algorithm.           
+#We use Rabin-Karp algorithm for acoomplishing our string matching algorithm.           
 #The advantage of the Kmp algorithm is that it only requires O(m) and processing time, while the fastest finite state automaton also requires O(m * | Ʃ |)
 
 
@@ -36,37 +36,13 @@ class KMP:
                     q = 0
 
 
+#Test case 1: Input string "of" from "Pirates of the Caribbean" -> output: 9
+#Comment: 
+#Let's assume that the prefix function has been obtained, that is, we don't care about how the prefix function is calculated. Then when we already have a way to get the prefix function, the next step is to match the pattern P.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Line 6 calls the function longest_prefix_suffix to calculate the prefix function of mode P. 
+#Line 8 begins to scan the text T sequentially, and note that the variable q records the number of characters that successfully match the pattern P at this moment. 
+#When the next character fails to match (P[q+1]!=T[i]), the value of q is recalculated according to the prefix function, such as the two lines of code 9 and 10. 
+#The value of q is simply incremented by 1 when a match is successful. In the last 13 lines, when the value of q (the number of characters already matched) is equal to the length of the pattern P, we have found a matching string.
 
 
